210b4abd5335d77e7f5c2c72c3f08fdc175a3fcc
Connor Meth <connormeth27@gmail.com>
2019-09-13 21:09:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,8 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.each do |string|
-  new_array << string.reverse 
-  
+  new_array << string.reverse
+  end
+  new_array.join
 end
 reverse_each_word("Hello there, and how are you?")
